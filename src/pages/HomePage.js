814b3c513b7aedb14723676cdc8c771de814e371
asadbek-dev <asadbek.dev1101@gmail.com>
2020-11-24 22:12:00 +0500

    my first react file

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const HomePage=()=>(
+    <>
+     <h1>Hello,welcome to my blog!</h1>
+    <p>
+        Asadbek Sobirov Asadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek Sobirov
+        Asadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek Sobirov
+    </p>
+    </>
+    
+);
+export default HomePage;
