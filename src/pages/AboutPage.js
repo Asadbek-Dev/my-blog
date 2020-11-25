c739380fd0f26d7d9c4cffa3a131c3d677d950d2
asadbek-dev <asadbek.dev1101@gmail.com>
2020-11-25 15:33:06 +0500

    in AboutPage added h2

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,11 +2,11 @@ import React from 'react';
 const AboutPage=()=>(
     <>
      <h1>About me!</h1>
-    <p>
-        Asadbek Sobirov Asadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek Sobirov
-        Asadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek Sobirov
+     <p>
+        My name is Asadbek, I am 20 years old and I am currently a 2nd year student at Urgench State University.
+        The direction is mathematics and software of information systems and I have been preparing, studying and studying web programming since September 2020.
     </p>
-    <h2>Assalomu aleykum Xush Kelibsiz!</h2>
+    <h2>My teachers who have been teaching me web programming so far are Zafar Sabirov and Ahmadjon Abdusamadov. I thank them very much.</h2>
     </>
     
 );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,8 @@ const HomePage=()=>(
     <>
      <h1>Hello,welcome to my blog!</h1>
     <p>
-        Asadbek Sobirov Asadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek Sobirov
-        Asadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek Sobirov
+        My name is Asadbek, I am 20 years old and I am currently a 2nd year student at Urgench State University.
+        The direction is mathematics and software of information systems and I have been preparing, studying and studying web programming since September 2020.
     </p>
     <h2>Assalomu aleykum Xush Kelibsiz!</h2>
     </>
