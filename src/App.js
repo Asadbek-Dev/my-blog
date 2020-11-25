3f08305b30f71e163c221ce16054fbfb30a7899d
asadbek-dev <asadbek.dev1101@gmail.com>
2020-11-25 14:27:31 +0500

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
+import NavBar from './NavBar';
 import './App.css'
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
+class App extends Component {
+  render() {
+      return (
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage}/>
+          <Route path="/articles-list" component={ArticlesList}/>
+          <Route path="/article" component={ArticlePage}/>
+        </div>
+      </div>
+    </Router>
   );
+
+  }
 }
 
 export default App;
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const AboutPage=()=>(
+    <>
+     <h1>About me!</h1>
+    <p>
+        Asadbek Sobirov Asadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek Sobirov
+        Asadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek SobirovAsadbek Sobirov
+    </p>
+    <h2>Assalomu aleykum Xush Kelibsiz!</h2>
+    </>
+    
+);
+export default AboutPage;
